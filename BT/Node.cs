b0d28efb41e181d3a5b10be864ba455a520da767
Sany-SHIriCE <Sany-SHIriCE@402D-2>
2023-05-11 11:59:49 +0700

    Добавьте файлы проекта.

---
+++ BT/Node.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BT
+{
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public Node root;
+
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int key)
+        {
+            root = Insert(root, key);
+        }
+
+        Node Insert(Node root, int key)
+        {
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            if (key < root.data)
+            {
+                root.left = Insert(root.left, key);
+            }
+            else if (key > root.data)
+            {
+                root.right = Insert(root.right, key);
+            }
+
+            return root;
+        }
+
+        public void InOrder()
+        {
+            InOrder(root);
+        }
+
+        void InOrder(Node root)
+        {
+            if (root != null)
+            {
+                InOrder(root.left);
+                Console.Write(root.data + " ");
+                InOrder(root.right);
+            }
+        }
+    }
+}
---
+++ BT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            // Узлы
+            tree.Insert(10);
+            tree.Insert(50);
+            tree.Insert(80);
+            tree.Insert(40);
+            tree.Insert(90);
+            tree.Insert(60);
+            tree.Insert(70);
+
+            // Содержимое дерева
+            Console.Write("Попорядковый обход: ");
+            tree.InOrder();
+            Console.WriteLine();
+        }
+    }
+}
